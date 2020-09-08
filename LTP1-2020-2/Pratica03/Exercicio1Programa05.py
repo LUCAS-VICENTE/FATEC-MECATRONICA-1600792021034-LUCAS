a4530d7489e4981026fe6f3b9acea8d63cdc785c
LUCAS-VICENTE <70581673+LUCAS-VICENTE@users.noreply.github.com>
2020-09-08 12:40:52 -0300

    Update Exercicio1Programa05.py

--- LTP1-2020-2/Pratica03/Exercicio1Programa05.py
+++ LTP1-2020-2/Pratica03/Exercicio1Programa05.py
@@ -1,4 +1,4 @@
 #Exercicio 1
 temp_celsius = float(input('C:'))
-temp_kelvin = temp_celsius + 273.15
-print('O valor de ', temp_celsius, 'em Kelvin eh:', temp_kelvin)
+temp_kelvin = temp_celsius + 273
+print('O valor de', temp_celsius, 'em kelvin e:', temp_kelvin)
