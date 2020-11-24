b84899831b1094402e675e99094cc386adda55ae
LUCAS-VICENTE <70581673+LUCAS-VICENTE@users.noreply.github.com>
2020-11-24 09:20:25 -0300

    Create Prog03.py

---
+++ LTP1-2020-2/Pratica13/Prog03.py
@@ -0,0 +1,9 @@
+num1 = flutuação(entrada("Informe um valor:"))
+num2 = flutuação(entrada("Informe outro valor:"))
+
+se num1 > num2:
+  impressão("Maior:", num1)
+elif num2 > num1:
+  impressão("Maior:", num2)
+outra coisa:
+  impressão("São Iguais")
