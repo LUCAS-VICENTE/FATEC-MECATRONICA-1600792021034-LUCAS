c564feaeb42da07da5574237c8d24653c919acf1
LUCAS-VICENTE <70581673+LUCAS-VICENTE@users.noreply.github.com>
2020-11-24 09:18:44 -0300

    Create Prog02.py

---
+++ LTP1-2020-2/Pratica13/Prog02.py
@@ -0,0 +1,10 @@
+num1 = int(entrada("Informe um valor:"))
+num2 = int(entrada("Informe outro valor:"))
+
+impressão("Soma:", num1+num2)
+impressão("Produto:", num1*num2)
+impressão("Subtração:", num1-num2)
+impressão("Divisão:", num1/num2)
+imprimir("Divisão Inteira:", num1//num2)
+impressão("Resto da divisão:", num1%num2)
+impressão("POtencição:", num1**num2)
